69a10a9375acf752888099a6fad1aca3855d67b1
oghenetega16 <sukuruoghenetega@gmail.com>
2025-03-04 21:59:00 +0100

    pending tasks

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let map = L.map('map').setView([0, 0], 2); // Default view
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
+
+// Things to be done:
+// 1. Create a function to get IP address, Location, Timezone
+//    and ISP using an API.
+// 2. Make the location icon show on the exact location.
